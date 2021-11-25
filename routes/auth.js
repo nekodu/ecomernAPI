aa4071cf04e1ba29e8d4b8a16cdabff491ca88d5
nekodu <kaplangokce@yandex.com>
2021-11-25 05:18:20 +0300

    jason web token integration has completed

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const router = require("express").Router();
 
 const CryptoJS= require("crypto-js")
 
+const jwt = require("jsonwebtoken")
+
 //REGISTER
 
 router.post("/register", async (req,res)=>{
@@ -42,10 +44,20 @@ router.post("/login", async (req,res)=>{
 
         OriginaPassword !== req.body.password && res.status(401).json("Wrong Credentials");
 
+
+            const accessToken = jwt.sign(
+                {
+                id:user._id, 
+                isAdmin: user.isAdmin,
+                }, 
+                process.env.JWT_SEC,
+             {expiresIn:"2d"}
+            );
+
             const{password, ...others} = user._doc ;
 
 
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
     }catch(err){
         res.status(500).json(err);
     }
